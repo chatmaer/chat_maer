eebbebf3ed94d762108829b946429593fef60730
chatmaer <chatmaer0102@gmail.com>
2025-11-22 18:44:20 -0500

    add cors vercel domain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,6 +30,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:3000",
   "http://127.0.0.1:5173",
+  "https://chat-maer.vercel.app",
   // Add custom domains from environment variable
   ...config.allowedOrigins,
 ];
